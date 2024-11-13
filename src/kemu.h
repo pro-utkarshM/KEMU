e009656d10feca9b8bf1b3afb8679d32fbd039fb
Utkarsh Maurya <projects.utkarshMaurya@gmail.com>
2024-11-13 22:32:55 +0530

    naming type cast macros

--- src/kemu.h
+++ src/kemu.h
@@ -13,10 +13,10 @@ typedef unsigned short int int16;
 typedef unsigned int int32;
 typedef unsigned long long int int64;
 
-#define $8 (int8 *)
-#define $6 (int16)
-#define $2 (int32)
-#define $4 (int64)
+#define $1 (int8 *)
+#define $2 (int16)
+#define $4 (int32)
+#define $8 (int64)
 #define $c (char *)
 #define $i (int)
 
